5b2ebfaddc8580ef8391be054aa4d137a1f3bf36
maria-osokina <info@aviatus.ru>
2021-07-27 15:22:24 +0300

    interface

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ print("connecting to the robot...")
 #p = Platform('192.168.12.20') # подключение к платформе | закомментировать если платформа не включена
 platform_action = 0  # 0-stop/1-forward/2-backward/3-left/4-right
 
-UPLOAD_FOLDER = '/home/main/upload_server' # папка куда сохраняются загруженные картинки
+#UPLOAD_FOLDER = '/home/main/upload_server' # папка куда сохраняются загруженные картинки
+UPLOAD_FOLDER = 'c:\\_work\\projects\\drawing-robot\\repos\\drawing-robot\\temp\\' # папка куда сохраняются загруженные картинки
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp'} # допустимые форматы изображений
 
 app = Flask(__name__) # инициализируем фласк
@@ -88,6 +89,12 @@ def preview(): # страничка предпросмотра
 
     filename = os.path.join('static', "preview.jpg")
     return render_template('preview.html', preview_image=filename)
+
+
+@app.route('/preview-src', methods=['GET', 'POST'])
+def preview_src(): # страничка предпросмотра
+    filename = os.path.join('static', "preview.jpg")
+    return filename
 
 
 @app.route('/control', methods=['GET', 'POST'])
